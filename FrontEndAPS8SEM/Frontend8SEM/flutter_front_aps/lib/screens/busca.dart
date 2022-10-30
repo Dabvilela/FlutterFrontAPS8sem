b0891ab593fc26e880ca1b6152fecf8e3c527e29
Daniel A B Vilela <dabvilela@gmail.com>
2022-10-30 00:21:57 -0300

    Adicionando dropDownlist e actionFloatingButtton na tela de busca.

--- FrontEndAPS8SEM/Frontend8SEM/flutter_front_aps/lib/screens/busca.dart
+++ FrontEndAPS8SEM/Frontend8SEM/flutter_front_aps/lib/screens/busca.dart
@@ -8,7 +8,13 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  
+
+  final dropValueTipos = ValueNotifier('');
+  final dropOpcaoTipos = ['Eletronicos'];//array de tipos
+
+  final dropValueResiduos = ValueNotifier('');
+  final droOpcaoResiduos = ['Pilhas', 'Entulhos','Lixo Tóxico', 'Reciclavel'];// array de residuos
+
   @override
   Widget build(BuildContext context) {
 
@@ -22,20 +28,92 @@ class _SearchScreenState extends State<SearchScreen> {
       body: Container(
         
         padding: const EdgeInsets.only(
-          top: 80,
-          left: 40,
-          right: 40,
+          top: 30,
+          left: 20,
+          right: 20,
         ),
         
         color: Colors.greenAccent,
 
         child: ListView(
-  
-        ),
-        
-      ),
+          
+          children:[             
+            
+            Row(
+
+              children: [
+
+                //Ajustar para segundo dropdownlist
+                ValueListenableBuilder(
+                  valueListenable: dropValueTipos, builder: (BuildContext context, String value, _){
+                    return dropDownList(value, dropValueTipos, dropOpcaoTipos, 'Tipos');
+                  }               
+                ),
+
+                const Spacer(),
+
+                ValueListenableBuilder(
+                  valueListenable: dropValueResiduos, builder: (BuildContext context, String value, _){
+                    return dropDownList(value, dropValueResiduos, droOpcaoResiduos, 'Residuos');
+                  }               
+                ),
+
+                const Spacer(),
+
+                FloatingActionButton(
+
+                  backgroundColor: Colors.green,
+                  child: const Icon(Icons.search),
+                  onPressed: (){
+                   //criar funçao para consumir as API 
+                  },
+
+                )
+
+              ],
+            ),
+
+          ],
+                       
+        )
+
+      ),      
       
     );
+  }  
+
+  SizedBox dropDownList(String value, ValueNotifier<String> dropValue, List<String> dropOpcao, String hintText ) {
+    
+    return SizedBox(      
+
+      width:120,
+      
+      child:DropdownButton<String>(
+    
+        dropdownColor: Colors.greenAccent,
+        isExpanded: true,
+        hint: Text(hintText),
+        value: (value.isEmpty)? null : value,
+        
+        onChanged: (escolha)=> dropValue.value = escolha.toString(),
+            
+        items:dropOpcao.map((opcao) => DropdownMenuItem(
+          value: opcao,
+          child:Text(opcao),
+        )).toList(),
+        
+      ),
+
+    );
   }
+
+}
+
+class ViaCep{
+  int id = 0; 
+  
+}
+
+class Residuos{
 
 }
